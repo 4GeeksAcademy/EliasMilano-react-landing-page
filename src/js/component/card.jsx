c4fab18b55983d629023c3c1b65548fbff0a603d
Elías Milano <87205729+MilanoTester@users.noreply.github.com>
2024-10-12 19:44:33 +0000

    finished according with sample

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Card = () => {
     return (
         <div className="card text-center my-2" style={{width:"325px", height:"500px"}} >
-            <img src="..." className="card-img-top" alt="..." />
+            <img src="https://raw.githubusercontent.com/4GeeksAcademy/EliasMilano-react-landing-page/refs/heads/main/src/img/placehold-500x325.png" className="card-img-top" alt="placehold-500x325" />
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
             </div>
             <div className="card-footer bg-white">
                 <a href="#" className="btn btn-primary">Go somewhere</a>
@@ -14,6 +14,5 @@ const Card = () => {
         </div>
     )
 }
-
 
 export default Card;
--- src/js/component/hero.jsx
+++ src/js/component/hero.jsx
@@ -6,7 +6,8 @@ const Hero = () => {
             <div className="col-11 p-3 mx-auto mt-5 mb-2 bg-light rounded-3">
                 <div className="container-fluid py-5">
                     <h1 className="display-5 fw-semibold">A Warm Welcome!</h1>
-                    <p className="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+                    <p className="col-md-8 fs-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaret nam nobis illo aspernatur vitae fugiat numquam repellat.
+</p>
                     <button className="btn btn-primary btn-lg" type="button">Call to action!</button>
                 </div>
             </div>
